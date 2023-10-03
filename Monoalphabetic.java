73ffc2df98b9403506dbb8b581c775df03993fce
ShruthiRajamanickam <142685547+ShruthiRajamanickam@users.noreply.github.com>
2023-10-03 15:15:25 +0530

    Update and rename Main.java to Monoalphabetic.java

--- Monoalphabetic.java
+++ Monoalphabetic.java
@@ -1,5 +1,5 @@
 import java.io.*;
-public class Main {
+public class Monoalphabetic {
    public static char alphabeticalChars[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
    public static char mappingChars[] = { 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M' };
    public static String encryptStr(String str) {
